0f7b78b0dca588198e4ae6fa3da00c09bfea7191
Myawmyaw <eco.villaraza@gmail.com>
2025-02-07 18:20:08 +0800

    init: splash

---
+++ Mingming/Main.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Mingming
+//
+//  Created by Jerico Villaraza on 2/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isSplash = true
+    
+    var body: some View {
+        ZStack {
+            if isSplash {
+                Splash()
+            } else {
+                Home()
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                isSplash = false
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mingming/Screens/Splash.swift
@@ -0,0 +1,18 @@
+//
+//  Splash.swift
+//  Mingming
+//
+//  Created by Jerico Villaraza on 2/7/25.
+//
+
+import SwiftUI
+
+struct Splash: View {
+    var body: some View {
+        Image(.splashLogo)
+    }
+}
+
+#Preview {
+    Splash()
+}
