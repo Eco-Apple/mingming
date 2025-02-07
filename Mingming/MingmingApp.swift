20d1c915957f8364e0795cff37725185543f412c
Myawmyaw <eco.villaraza@gmail.com>
2025-02-07 15:54:18 +0800

    first commit

---
+++ Mingming/MingmingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MingmingApp.swift
+//  Mingming
+//
+//  Created by Jerico Villaraza on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MingmingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
