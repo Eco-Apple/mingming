7a61b5cd18983278ed8f81b50382bfa0b45232fa
Myawmyaw <eco.villaraza@gmail.com>
2025-04-01 16:00:41 +0800

    fix: delete ui

--- Mingming/Components/Delete.swift
+++ Mingming/Components/Delete.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct Delete: View {
+    var title: String
     var callback: (Bool) -> Void
     
     var body: some View {
         VStack(alignment: .leading, spacing: .zero) {
             (Text("Are you sure you want to delete ")
-                .font(.title3)
-            + Text("\nRunning")
+                .font(.body)
+            + Text("\n\(title)")
                 .font(.headline)
             + Text(" ?")
-                .font(.title3))
+                .font(.body))
             .multilineTextAlignment(.center)
             
             HStack {
@@ -25,6 +26,7 @@ struct Delete: View {
                     callback(true)
                 } label: {
                     Text("Yes")
+                        .font(.headline)
                 }
                 .frame(maxWidth: .infinity)
                 .buttonStyle(PlainButtonStyle())
@@ -33,6 +35,7 @@ struct Delete: View {
                     callback(false)
                 } label: {
                     Text("No")
+                        .font(.headline)
                 }
                 .frame(maxWidth: .infinity)
                 .buttonStyle(PlainButtonStyle())
@@ -42,7 +45,7 @@ struct Delete: View {
         }
         .padding(.vertical, 20)
         .padding(.horizontal, 30)
-        .frame(width: 340)
+        .frame(width: 300)
         .background(.white)
         .cornerRadius(20)
         .shadow(color: .black.opacity(0.25), radius: 6.3, x: 0, y: 0)
@@ -50,5 +53,5 @@ struct Delete: View {
 }
 
 #Preview {
-    Delete(callback: { _ in })
+    Delete(title: "Run", callback: { _ in })
 }
